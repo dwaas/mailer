d0070b998dce947f16fb5c893da999daedbd034c
Devin Waas <dsc.waas@gmail.com>
2020-05-18 17:06:28 +0200

    Support default Return-Path

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use log::Level;
 use structopt::StructOpt;
 use lettre::{SendableEmail, EmailAddress, Transport, Envelope, SmtpClient};
 
+use lettre_email::*;
+
 use std::io;
 use std::io::prelude::*;
 
@@ -24,7 +26,7 @@ fn main() {
     let mut from = "user@localhost".to_string();
     let mut cc = "".to_string();
     let mut bcc = "".to_string();
-    let to = "devin@localhost";
+    let to = "devin@localhost"; //FIXME
 
     let mut stdin = io::stdin();
     trace!("Parsing header.");
@@ -49,11 +51,10 @@ fn main() {
 
     trace!("Parsing body.");
     let mut msg = "".to_string();
-    stdin.read_to_string(&mut msg);
+    let _ = stdin.read_to_string(&mut msg);
     let msg = msg.trim();
     debug!("msg body: \n{}", msg);
-
-
+/*
     let email = SendableEmail::new(
         Envelope::new(
             Some(EmailAddress::new(from.to_string()).unwrap()),
@@ -62,6 +63,18 @@ fn main() {
             "id".to_string(), //TODO?
             msg.to_string().into_bytes(),
         );
+    */
+    let ret_path = "bounces@example.com";
+    let email = EmailBuilder::new()
+        .to(to.to_string())
+        .cc(cc)
+        .bcc(bcc)
+        .from(ret_path.to_string()) // Sets Return-Path as a side effect. // TODO, file bug.
+        .text(msg.to_string())
+        .header(("from", from.to_string()))
+        .build()
+        .expect("Can't compose mail")
+        .into();
 
     let mut mailer = SmtpClient::new_unencrypted_localhost().unwrap().transport();
 
